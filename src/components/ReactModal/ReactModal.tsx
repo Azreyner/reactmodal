7d48a2b7b2b240db1ce4f44b603d7561ea9b5e7b
unknown <matisse.99@hotmail.fr>
2023-08-18 16:10:26 +0200

    maj Read me et ajout doc

--- src/components/ReactModal/ReactModal.tsx
+++ src/components/ReactModal/ReactModal.tsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import "./reactModal.css";
 
+/**
+ * Ici sont définis les propset quel type de valeur est attendu de la part de chacun de ses props
+ * ContentModal : Contiendra le texte afficher dans la modale
+ * ContentButton : Contiendra le texte affiché dans le bouton de la modale
+ * OnClose : Attend une fonction depuis le composant parent qu'il l'appelera qui doit servir a fermer la modale
+ * backgroundColorButton (OPTIONNEL) : Attend un chaine de caractère qui doit indiquer la couleur de fond du bouton
+ */
 interface modalContent {
   contentModal: string;
   contentButton: string;
+  onClose: () => void;
+  backgroundColorButton?: string;
 }
 
-/*ReactModal.defaultProps = {
-  contentModal: "Ma modale",
-  contentButton: "Mon bouton",
-};*/
-
 function ReactModal(props: modalContent) {
+  const handleButtonClick = () => {
+    props.onClose(); // Appeler la fonction de fermeture du modal fournie par le parent
+  };
+
+  /**
+   * Utilisation de la couleur de fond fournie par les props ou la valeur par défau
+   */
+  const buttonStyle = {
+    backgroundColor: props.backgroundColorButton || "#646cff",
+  };
+
   return (
-    <div className="reactModal">
-      <h4>{props.contentModal}</h4>
-      <button>{props.contentButton}</button>
+    <div className="overlayModal">
+      <div className="reactModal">
+        <h4>{props.contentModal}</h4>
+        <button style={buttonStyle} onClick={handleButtonClick}>
+          {props.contentButton}
+        </button>
+      </div>
     </div>
   );
 }
