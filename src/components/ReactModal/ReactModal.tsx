27fe8ab830f4ad311ea492c1eabb54b87f323f38
unknown <matisse.99@hotmail.fr>
2023-08-18 16:48:22 +0200

    Correction fautes doc

--- src/components/ReactModal/ReactModal.tsx
+++ src/components/ReactModal/ReactModal.tsx
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import "./reactModal.css";
 
 /**
- * Ici sont définis les propset quel type de valeur est attendu de la part de chacun de ses props
- * ContentModal : Contiendra le texte afficher dans la modale
+ * Ici sont définis les props et quel type de valeur est attendu de la part de chacun de ses props
+ * ContentModal : Contiendra le texte affiché dans la modale
  * ContentButton : Contiendra le texte affiché dans le bouton de la modale
- * OnClose : Attend une fonction depuis le composant parent qu'il l'appelera qui doit servir a fermer la modale
+ * OnClose : Attend une fonction depuis le composant parent qui l'appellera qui servira a fermer la modale
  * backgroundColorButton (OPTIONNEL) : Attend un chaine de caractère qui doit indiquer la couleur de fond du bouton
  */
 interface modalContent {
@@ -17,11 +17,11 @@ interface modalContent {
 
 function ReactModal(props: modalContent) {
   const handleButtonClick = () => {
-    props.onClose(); // Appeler la fonction de fermeture du modal fournie par le parent
+    props.onClose(); // Appel de la fonction de fermeture de la modale fournie par le parent
   };
 
   /**
-   * Utilisation de la couleur de fond fournie par les props ou la valeur par défau
+   * Utilisation de la couleur de fond fournie par backgroundColorButton ou la valeur par défaut
    */
   const buttonStyle = {
     backgroundColor: props.backgroundColorButton || "#646cff",
